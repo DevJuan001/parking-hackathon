b1b1a905847104af3ca7a5778f8bb95fba6c1c06
DevJuan001 <juanesyt7@gmail.com>
2026-06-06 23:29:10 -0500

    feat(backend): add UserResponse and UserByIdResponse models for user data handling

---
+++ backend/app/features/users/models/users_responses.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr
+
+from app.utils.base_schema import BaseSchema
+
+
+class UserResponse(BaseModel):
+    role_id: int
+    role_name: str
+    id: int
+    name: str
+    first_surname: str
+    second_surname: str
+    email: EmailStr
+    created_at: str
+    status: int
+
+
+class UserByIdResponse(BaseModel):
+    role: str
+    id: int
+    name: str
+    first_surname: str
+    second_surname: str
+    email: EmailStr
+    created_at: str
+    status: int
