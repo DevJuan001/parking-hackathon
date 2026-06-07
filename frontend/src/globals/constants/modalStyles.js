4c1cd78c5cc79f0a1b75140733636e457bfa3f74
DevJuan001 <juanesyt7@gmail.com>
2026-06-07 16:16:43 -0500

    refactor(frontend): update menu modal style for improved layout and consistency

--- frontend/src/globals/constants/modalStyles.js
+++ frontend/src/globals/constants/modalStyles.js
@@ -1,6 +1,6 @@
 export const modal_styles = {
   select: "p-1 w-[350px] md:w-[200px]",
   calendar: "w-[365px] md:w-[400px]",
-  menu: "p-1 w-[250px]",
+  menu: "p-2 w-[210px]",
   default: "p-7 w-[400px] md:w-[500px]",
 };
