8e7f7f8e07ac5cbce5227cda4e62c330c4922284
DevJuan001 <juanesyt7@gmail.com>
2026-06-07 15:55:37 -0500

    fix(frontend): adjust width of select modal style for better responsiveness

--- frontend/src/globals/constants/modalStyles.js
+++ frontend/src/globals/constants/modalStyles.js
@@ -1,5 +1,5 @@
 export const modal_styles = {
-  select: "p-1 w-[350px] md:w-[400px]",
+  select: "p-1 w-[350px] md:w-[200px]",
   calendar: "w-[365px] md:w-[400px]",
   menu: "p-1 w-[250px]",
   default: "p-7 w-[400px] md:w-[500px]",
