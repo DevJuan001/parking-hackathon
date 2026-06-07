2e76a8179d145edfd8ff7226b04b4e893704c839
DevJuan001 <juanesyt7@gmail.com>
2026-06-07 01:10:09 -0500

    feat: add Navbar and NavbarMenuModal components for enhanced navigation

---
+++ frontend/src/globals/components/Layout/aside/NavbarMenuModal.jsx
@@ -0,0 +1,67 @@
+import Icon from "../../ui/Icon";
+import NavItem from "./NavItem";
+import Modal from "../../modals/Modal";
+import {
+  avatarItem,
+  secondSectionItems,
+} from "../../../constants/asideMenuItems";
+
+export default function NavbarMenuModal({
+  isOpen,
+  triggerRef,
+  onClose,
+  helpOnClick,
+  avatarOnClick,
+}) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      type={"menu"}
+      onClose={onClose}
+      triggerRef={triggerRef}
+    >
+      <button
+        onClick={avatarOnClick}
+        className="w-full h-full flex items-center py-2.5 px-4 gap-2.5 rounded-3xl transition duration-300
+        hover:bg-[#e5e7eb96]
+        dark:text-[#7E8088] dark:hover:bg-[#181818]"
+      >
+        <img src={avatarItem.icon} alt={avatarItem.alt} className="w-8 h-8" />
+      </button>
+
+      {secondSectionItems.map((item) =>
+        item.path ? (
+          <NavItem
+            showName={true}
+            itemId={`${item.itemId}`}
+            key={item.name}
+            path={item.path}
+            name={item.name}
+            icon={item.icon}
+          />
+        ) : (
+          <button
+            key={item.name}
+            onClick={helpOnClick}
+            className="w-full h-full flex items-center py-3.5 px-6 gap-2 rounded-full transition duration-300 cursor-pointer group text-[#75777eb7]
+            hover:cursor-pointer
+            hover:bg-[#e5e7eb96] 
+            dark:text-gray-50 dark:hover:bg-[#202022]"
+          >
+            <Icon
+              name={item.icon}
+              size={25}
+              className={`fill-none
+                group-hover:text-black group-hover:[--icon-weight:500]
+                dark:group-hover:text-white`}
+            />
+
+            <span className="font-medium group-hover:text-black dark:group-hover:text-white">
+              {item.name}
+            </span>
+          </button>
+        ),
+      )}
+    </Modal>
+  );
+}
