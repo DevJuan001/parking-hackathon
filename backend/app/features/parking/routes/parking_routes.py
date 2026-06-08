2a1de938a1315a1bead7a347b85183645c5291b4
DevJuan001 <juanesyt7@gmail.com>
2026-06-08 16:00:21 -0500

    refactor(parking): remove payment-related routes and dependencies from parking_routes.py

--- backend/app/features/parking/routes/parking_routes.py
+++ backend/app/features/parking/routes/parking_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from fastapi_limiter.depends import RateLimiter
+from app.features.parking.models.parking_schemas import CreatePlateSchema
 from app.features.parking.controllers.parking_controller import ParkingController
-from app.features.parking.models.parking_schemas import CreatePlateSchema, CreatePaymentSchema, ParkingFiltersSchema
 
 router = APIRouter(
     prefix="/api/parking",
@@ -29,26 +29,6 @@ def get_all_spots():
     return ParkingController.get_all_spots()
 
 
-@router.get(
-    "/payments",
-    dependencies=[
-        Depends(RateLimiter(times=30, seconds=60))
-    ]
-)
-def get_all_payments(filters: ParkingFiltersSchema = Depends()):
-    return ParkingController.get_all_payments(filters)
-
-
-@router.get(
-    "/payments/{payment_id}",
-    dependencies=[
-        Depends(RateLimiter(times=30, seconds=60))
-    ]
-)
-def get_payment_by_id(payment_id: int):
-    return ParkingController.get_payment_by_id(payment_id)
-
-
 @router.post(
     "/plates/create",
     dependencies=[
@@ -59,15 +39,3 @@ async def create_plate(
     plate_data: CreatePlateSchema
 ):
     return await ParkingController.create_plate(plate_data)
-
-
-@router.post(
-    "/payments/create",
-    dependencies=[
-        Depends(RateLimiter(times=30, seconds=60))
-    ]
-)
-async def create_payment(
-    payment_data: CreatePaymentSchema
-):
-    return await ParkingController.create_payment(payment_data)
