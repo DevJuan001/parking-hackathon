cfebc4b7674b45e2d9a7736899462463f103a66d
DevJuan001 <juanesyt7@gmail.com>
2026-06-09 15:19:34 -0500

    feat(homePage): integrate modals for editing and creating parking spots in HomePage component

--- frontend/src/modules/home/HomePage.jsx
+++ frontend/src/modules/home/HomePage.jsx
@@ -3,6 +3,10 @@ import { useModal } from "../../globals/hooks/useModal";
 // Components
 import Layout from "../../globals/components/Layout/Layout";
 import HomeSectionsContainer from "./components/ui/HomeSectionsContainer";
+import Modal from "../../globals/components/modals/Modal";
+// Modals
+import EditSpotModal from "./components/modals/EditSpotModal";
+import CreateSpotModal from "./components/modals/CreateSpotModal";
 
 export default function HomePage() {
   const { isOpen, modalType, modalData, triggerRef, openModal, closeModal } =
@@ -10,7 +14,30 @@ export default function HomePage() {
 
   return (
     <Layout avatarOnClick={(e) => openModal(null, "avatar", e.currentTarget)}>
-      <HomeSectionsContainer />
+      <HomeSectionsContainer openModal={openModal} />
+
+      {modalType && (
+        <Modal
+          isOpen={isOpen}
+          title={
+            modalType === "edit"
+              ? "Editar Plaza"
+              : modalType === "create"
+                ? "Agregar Plaza"
+                : ""
+          }
+          onClose={closeModal}
+          location="anchored"
+          growDirection="center"
+          triggerRef={triggerRef}
+        >
+          {modalType === "edit" && (
+            <EditSpotModal onClose={closeModal} spot={modalData} />
+          )}
+
+          {modalType === "create" && <CreateSpotModal onClose={closeModal} />}
+        </Modal>
+      )}
     </Layout>
   );
 }
