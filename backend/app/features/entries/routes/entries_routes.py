9f3d2e0eb20f119f1a8dca47991ad7d522030839
DevJuan001 <juanesyt7@gmail.com>
2026-06-09 13:31:48 -0500

    feat(entries): integrate JWT verification into entry routes for enhanced security and data access

--- backend/app/features/entries/routes/entries_routes.py
+++ backend/app/features/entries/routes/entries_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi_limiter.depends import RateLimiter
+from app.middlewares.jwt_middleware import verify_jwt
 from app.middlewares.roles_middleware import require_roles
 from app.features.entries.controllers.entries_controller import EntriesController
 from app.features.entries.models.entries_schemas import CreateEntrySchema, EntriesFiltersSchema
@@ -14,42 +15,56 @@ router = APIRouter(
     "/",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin"])),
+        Depends(verify_jwt)
     ]
 )
-def get_all_entries(filters: EntriesFiltersSchema = Depends()):
-    return EntriesController.get_all_entries(filters)
+def get_all_entries(
+    filters: EntriesFiltersSchema = Depends(),
+    payload: dict = Depends(verify_jwt)
+):
+    return EntriesController.get_all_entries(filters, payload)
 
 
 @router.get(
     "/plate/{plate_id}",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin"])),
+        Depends(verify_jwt)
     ]
 )
-def get_entries_by_plate(plate_id: int):
-    return EntriesController.get_entries_by_plate(plate_id)
+def get_entries_by_plate(
+    plate_id: int,
+    payload: dict = Depends(verify_jwt)
+):
+    return EntriesController.get_entries_by_plate(plate_id, payload)
 
 
 @router.get(
     "/{entry_id}",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin"])),
+        Depends(verify_jwt)
     ]
 )
-def get_entry_by_id(entry_id: int):
-    return EntriesController.get_entry_by_id(entry_id)
+def get_entry_by_id(
+    entry_id: int,
+    payload: dict = Depends(verify_jwt)
+):
+    return EntriesController.get_entry_by_id(entry_id, payload)
 
 
 @router.post(
     "/create",
     dependencies=[
-        Depends(RateLimiter(times=30, seconds=60))
+        Depends(RateLimiter(times=30, seconds=60)),
+        Depends(verify_jwt)
     ]
 )
 async def create_entry(
-    entry_data: CreateEntrySchema
+    entry_data: CreateEntrySchema,
+    payload: dict = Depends(verify_jwt)
 ):
-    return await EntriesController.create_entry(entry_data)
+    return await EntriesController.create_entry(entry_data, payload)
