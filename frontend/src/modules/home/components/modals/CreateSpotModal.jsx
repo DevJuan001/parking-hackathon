9ec6ba42278da820f2d1ecc10735c04d1b57fb7f
DevJuan001 <juanesyt7@gmail.com>
2026-06-09 15:19:44 -0500

    feat(modals): implement CreateSpotModal and EditSpotModal for managing parking spots

---
+++ frontend/src/modules/home/components/modals/CreateSpotModal.jsx
@@ -0,0 +1,61 @@
+// Hooks
+import { useCreateSpot } from "../../hooks/useCreateSpot";
+import { useInnerModal } from "../../../../globals/hooks/useInnerModal";
+// Components
+import Loader from "../../../../globals/components/ui/Loader";
+import FormField from "../../../../globals/components/ui/FormField";
+import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+// Modals
+import ErrorModal from "../../../../globals/components/modals/ErrorModal";
+import SuccessModal from "../../../../globals/components/modals/SuccessModal";
+
+export default function CreateSpotModal({ onClose }) {
+  const { innerType, innerTrigger, openInnerModal } = useInnerModal();
+  const { handleChange, handleSubmit, spotData, loading } = useCreateSpot();
+
+  return (
+    <section className="flex flex-col items-center gap-2">
+      <FormField
+        id={"spot"}
+        name={"spot"}
+        labelText={"Plaza"}
+        value={spotData.spot}
+        onChange={handleChange}
+        autoComplete="off"
+      />
+
+      <ConfirmCancelButtons
+        confirmText={loading ? <Loader /> : "Crear"}
+        confirmButtonOnClick={(e) => handleSubmit(e, openInnerModal)}
+        cancelButtonOnClick={onClose}
+      />
+
+      {innerType === "success" && (
+        <SuccessModal
+          triggerRef={innerTrigger}
+          isOpen={true}
+          confirmTitle={"Plaza creada con éxito!"}
+          confirmText={
+            "La plaza se ha registrado correctamente, toca el botón de volver a la pagina para verlo"
+          }
+          confirmButtonText={"Volver a la pagina"}
+          onClose={() => {
+            openInnerModal(null);
+            onClose();
+          }}
+        />
+      )}
+
+      {innerType === "error" && (
+        <ErrorModal
+          triggerRef={innerTrigger}
+          isOpen={true}
+          errorTitle="¡No se pudo crear la plaza!"
+          errorText="Verifica que el nombre sea válido y vuelve a intentarlo"
+          confirmButtonText="Volver a intentarlo"
+          onClose={() => openInnerModal(null)}
+        />
+      )}
+    </section>
+  );
+}
