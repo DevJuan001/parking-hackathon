57b1e2de65d7c271f57387ada87b6cb2d7235330
DevJuan001 <juanesyt7@gmail.com>
2026-06-09 15:19:48 -0500

    feat(homeSections): integrate parking spot data and modal functionality in HomeSectionsContainer

--- frontend/src/modules/home/components/ui/HomeSectionsContainer.jsx
+++ frontend/src/modules/home/components/ui/HomeSectionsContainer.jsx
@@ -1,8 +1,13 @@
+// Hooks
+import { useParkingPlaces } from "../../hooks/useParkingPlaces";
+// Components
 import ParkingPlace from "./ParkingPlace";
 import SelectMenu from "../../../../globals/components/modals/SelectMenu";
 import Icon from "../../../../globals/components/ui/Icon";
 
-export default function HomeSectionsContainer() {
+export default function HomeSectionsContainer({ openModal }) {
+  const { spots } = useParkingPlaces();
+
   return (
     <section className="h-full w-full grid grid-cols-2 grid-rows-2 gap-4 pb-2">
       <div className="h-full w-full px-7 py-6 col-span-1 row-span-2 border-3 border-[#EBE6E7] rounded-[50px] dark:text-white">
@@ -21,9 +26,19 @@ export default function HomeSectionsContainer() {
           className="h-full w-full grid grid-cols-8 grid-rows-7 pt-2 gap-2
           xl:gap-5"
         >
-          <ParkingPlace name={"123"} status={"Disponible"} />
+          {spots.map((spot) => (
+            <ParkingPlace
+              key={spot.spot_id}
+              name={spot.spot}
+              status={spot.spot_status}
+              onClick={(e) => openModal(spot, "edit", e.currentTarget)}
+            />
+          ))}
 
-          <button className="h-full w-full flex flex-col items-center justify-center gap-2 border-2 rounded-3xl">
+          <button
+            onClick={(e) => openModal(null, "create", e.currentTarget)}
+            className="h-full w-full flex flex-col items-center justify-center gap-2 border-2 rounded-3xl hover:bg-[#efedf0] transition-colors dark:hover:bg-[#ffffff15]"
+          >
             <Icon name={"add"} size={35} />
           </button>
         </div>
