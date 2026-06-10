f691b3e350288e06433b2cac49cce2c335d693d1
DevJuan001 <juanesyt7@gmail.com>
2026-06-09 19:32:03 -0500

    feat(Earnings): add EarningsChart and EarningsPanel components for displaying earnings data

---
+++ frontend/src/modules/home/components/ui/EarningsChart.jsx
@@ -0,0 +1,9 @@
+import { PieChart, ResponsiveContainer } from "recharts";
+
+export default function EarningsChart() {
+  return (
+    <ResponsiveContainer width={"100%"} height={"100%"}>
+      <PieChart></PieChart>
+    </ResponsiveContainer>
+  );
+}
