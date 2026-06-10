7619e87a626710022e7692c87f8e923fdef068a2
DevJuan001 <juanesyt7@gmail.com>
2026-06-09 19:31:44 -0500

    feat(CreateButton): add new CreateButton component with customizable icon, text, and styles

---
+++ frontend/src/globals/components/ui/CreateButton.jsx
@@ -0,0 +1,28 @@
+import Icon from "./Icon";
+
+export default function CreateButton({ icon, text, borderRadius, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{ borderRadius: borderRadius }}
+      className="flex items-center px-3.5 py-5 gap-2 bg-black transition duration-500
+      lg:rounded-3xl lg:py-3
+      dark:bg-white dark:hover:shadow-[0px_0px_32px_-11px_#ffffff]"
+    >
+      <Icon
+        name={icon ? icon : "add"}
+        weight={600}
+        className="invert dark:brightness-200 dark:invert-0"
+      />
+
+      {text && (
+        <span
+          data-flip-id="modal-title"
+          className="hidden text-white font-medium dark:text-black lg:block"
+        >
+          {text}
+        </span>
+      )}
+    </button>
+  );
+}
