f4675714882f77e3c423b3643797702178039b01
DevJuan001 <juanesyt7@gmail.com>
2026-06-09 21:54:24 -0500

    refactor(SpotsPanel): streamline component structure by removing duplicate imports and reorganizing layout; enhance UI with a new flexbox design for better responsiveness

--- frontend/src/modules/parking/components/ui/SpotsPanel.jsx
+++ frontend/src/modules/parking/components/ui/SpotsPanel.jsx
@@ -1,4 +1,6 @@
 // Hooks
+import { useFloors } from "../../hooks/useFloors";
+import { useFilterSpots } from "../../hooks/useFilterSpots";
 import { useParkingSpots } from "../../hooks/useParkingSpots";
 // Components
 import Icon from "../../../../globals/components/ui/Icon";
@@ -6,8 +8,6 @@ import Skeleton from "../../../../globals/components/ui/Skeleton";
 // Constants
 import SpotItem from "./SpotItem";
 import SelectMenu from "../../../../globals/components/modals/SelectMenu";
-import { useFloors } from "../../hooks/useFloors";
-import { useFilterSpots } from "../../hooks/useFilterSpots";
 
 export default function SpotsPanel({ openModal }) {
   const { spots, loading } = useParkingSpots();
@@ -17,7 +17,23 @@ export default function SpotsPanel({ openModal }) {
   const isFirstLoad = spots.length === 0 && loading;
 
   return (
-    <div className="h-full w-full px-7 py-6 col-span-1 row-span-2 border-3 border-[#EBE6E7] rounded-[50px] dark:text-white">
+    <div className="h-full w-full px-7 py-6 flex flex-col gap-1 col-span-1 row-span-2 border-3 border-[#EBE6E7] rounded-[50px] dark:text-white">
+      <div className="flex items-center justify-between">
+        <span className="font-semibold">Plazas</span>
+
+        <SelectMenu
+          id={"floors-menu"}
+          name={"floor_id"}
+          value={filters.floor_id}
+          onChange={handleChange}
+          miniVersion={true}
+          options={floors.map((floor) => ({
+            value: floor.id,
+            label: `Piso ${floor.floor_number}`,
+          }))}
+        />
+      </div>
+
       {noSpots && (
         <div className="w-full h-full flex flex-col items-center justify-center gap-4 text-[#75777E]">
           <Icon name={"border_clear"} size={90} />
@@ -25,24 +41,6 @@ export default function SpotsPanel({ openModal }) {
           <span className="text-xl font-semibold">
             Aún no hay plazas registradas
           </span>
-        </div>
-      )}
-
-      {!noSpots && (
-        <div className="flex items-center justify-between">
-          <span className="font-semibold">Plazas</span>
-
-          <SelectMenu
-            id={"floors-menu"}
-            name={"floor_id"}
-            value={filters.floor_id}
-            onChange={handleChange}
-            miniVersion={true}
-            options={floors.map((floor) => ({
-              value: floor.id,
-              label: `Piso ${floor.floor_number}`,
-            }))}
-          />
         </div>
       )}
 
@@ -60,7 +58,7 @@ export default function SpotsPanel({ openModal }) {
           <div
             className="h-full w-full grid grid-cols-8 grid-rows-7 pt-2 gap-2
             lg:gap-x-2 lg:gap-y-4
-            2xl:gap-5"
+            2xl:gap-2.5"
           >
             {spots.map((spot) => (
               <SpotItem
