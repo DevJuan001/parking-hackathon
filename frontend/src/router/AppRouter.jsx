3c3316c887a6f17a423533dbc26af977f2307fbe
DevJuan001 <juanesyt7@gmail.com>
2026-06-14 17:32:49 -0500

    feat(frontend): update routing to use LandingPage as the main entry point

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import LoginPage from "../modules/login/LoginPage";
 import ProtectedRoutes from "./ProtectedRoutes";
 import { routesConfig } from "./constants/routesConfig";
+import LandingPage from "../modules/landing/LandingPage";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 export default function AppRouter() {
   return (
     <Routes>
       {/* Ruta no existente lo que hace es enviarlo al login */}
-      <Route path="*" element={<Navigate to="/login" />} />
+      <Route path="*" element={<Navigate to="/" />} />
 
       {/* Página Login o de Inicio de Sesión */}
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<LandingPage />} />
 
       {routesConfig.map(({ path, component: Component, roles }) => (
         <Route key={path} element={<ProtectedRoutes roles={roles} />}>
