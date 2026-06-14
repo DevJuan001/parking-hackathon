fc79fd8c3cb4648249592c42d8e4e90955419eee
DevJuan001 <juanesyt7@gmail.com>
2026-06-14 17:33:21 -0500

    feat(frontend): implement RegisterModal component for user registration

---
+++ frontend/src/modules/landing/components/modals/RegisterModal.jsx
@@ -0,0 +1,106 @@
+/// Hooks
+import { useRegister } from "../../hooks/useRegister";
+import { useInnerModal } from "../../../../globals/hooks/useInnerModal";
+// Componentes
+import Icon from "../../../../globals/components/ui/Icon";
+import Loader from "../../../../globals/components/ui/Loader";
+import LoginAndRegisterButtons from "./LoginAndRegisterButtons";
+import FormField from "../../../../globals/components/ui/FormField";
+// Modales
+import ErrorModal from "../../../../globals/components/modals/ErrorModal";
+
+export default function RegisterModal() {
+  const {
+    form,
+    loading,
+    error,
+    fieldError,
+    showPassword,
+    setShowPassword,
+    handleChange,
+    handleSubmit,
+  } = useRegister();
+  const { innerType, innerTrigger, openInnerModal, closeInnerModal } =
+    useInnerModal();
+
+  return (
+    <section className="flex flex-col gap-3 px-52 py-32 font-poppins">
+      <div className="flex gap-1 font-inter">
+        <Icon name={"parking_sign"} fill size={24} className={"dark:invert"} />
+
+        <span className="font-semibold dark:text-white">Parking</span>
+      </div>
+
+      <span className="text-4xl font-medium">Crea tu cuenta</span>
+
+      <form
+        action={(e) => handleChange(e, openInnerModal)}
+        className="flex flex-col gap-2"
+      >
+        <FormField
+          id={"email"}
+          name={"email"}
+          labelText={"Correo"}
+          value={form.email}
+          onChange={handleChange}
+          placeholder={"Escribe tu correo"}
+          className={fieldError("email")}
+        />
+
+        <FormField
+          id={"password"}
+          name={"password"}
+          labelText={"Contraseña"}
+          type={showPassword ? "text" : "password"}
+          value={form.password}
+          onChange={handleChange}
+          placeholder={"Escribe tu contraseña"}
+          className={fieldError("password")}
+        >
+          <button
+            className="flex items-center pr-1"
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+          >
+            <Icon name={showPassword ? "visibility_off" : "visibility"} />
+          </button>
+        </FormField>
+
+        <FormField
+          id={"repeat_password"}
+          name={"repeat_password"}
+          labelText={"Repite tu contraseña"}
+          type={showPassword ? "text" : "password"}
+          value={form.repeat_password}
+          onChange={handleChange}
+          placeholder={"Repite tu contraseña"}
+          className={fieldError("password")}
+        >
+          <button
+            className="flex items-center pr-1"
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+          >
+            <Icon name={showPassword ? "visibility_off" : "visibility"} />
+          </button>
+        </FormField>
+        
+        <LoginAndRegisterButtons
+          confirmButtonOnClick={(e) => handleSubmit(e, openInnerModal)}
+          confirmButtonText={loading ? <Loader /> : "Crear cuenta"}
+        />
+      </form>
+
+      {innerType === "error" && (
+        <ErrorModal
+          isOpen={true}
+          triggerRef={innerTrigger}
+          errorTitle={"No se pudo crear tu cuenta"}
+          errorText={error}
+          location="center"
+          onClose={closeInnerModal}
+        />
+      )}
+    </section>
+  );
+}
