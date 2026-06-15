c2879f139877149e7726dbc7710ac3189e8ba69c
DevJuan001 <juanesyt7@gmail.com>
2026-06-14 21:21:04 -0500

    style(frontend): add image to VideoSection for enhanced visual appeal

--- frontend/src/modules/landing/components/ui/VideoSection.jsx
+++ frontend/src/modules/landing/components/ui/VideoSection.jsx
@@ -1,7 +1,13 @@
 export default function VideoSection() {
   return (
     <section className="w-full flex flex-col items-center">
-      <div className="w-7xl h-[800px] bg-[#101012] rounded-3xl"></div>
+      <div className="w-7xl h-[800px] rounded-3xl">
+        <img
+          src="/public/parking-1.png"
+          alt="Video demostrativo del sistema"
+          className="rounded-3xl"
+        />
+      </div>
     </section>
   );
 }
