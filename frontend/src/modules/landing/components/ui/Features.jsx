0dbee4ef083ec24b56f0246ce904aa837cdc8296
DevJuan001 <juanesyt7@gmail.com>
2026-06-14 21:20:44 -0500

    style(frontend): refine Features component layout and styling for improved visual consistency

--- frontend/src/modules/landing/components/ui/Features.jsx
+++ frontend/src/modules/landing/components/ui/Features.jsx
@@ -2,14 +2,28 @@ import { features } from "../../data/features";
 
 export default function Features() {
   return (
-    <section id="#features" className="w-full flex flex-col gap-4 items-center">
-      <span className="font-medium dark:text-[#e4e2e5]">Características</span>
+    <section
+      id="features"
+      className="mt-10 w-full flex flex-col gap-4 items-center"
+    >
+      <span
+        className="font-medium
+        dark:text-[#e4e2e5]"
+      >
+        Características
+      </span>
 
-      <span className="text-5xl font-semibold dark:text-[#e4e2e5]">
+      <span
+        className="text-5xl text-[#1b1b1e] font-semibold
+        dark:text-[#e4e2e5]"
+      >
         Potencia tu operación
       </span>
 
-      <span className="max-w-xl text-xl text-center font-medium dark:text-[#7E8088]">
+      <span
+        className="max-w-xl text-xl text-center text-[#75777e] font-medium
+        dark:text-[#7E8088]"
+      >
         Todo lo que necesitas para gestionar tu parqueadero de forma eficiente
         desde una sola plataforma.
       </span>
@@ -17,8 +31,9 @@ export default function Features() {
       <div className="mt-5 w-[1500px] flex flex-col px-1 gap-2 rounded-full">
         {features.map((feature, index) => (
           <div
-            className={`w-full flex ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"} p-2 bg-[#101012] rounded-3xl
-            dark:text-white`}
+            key={feature.name}
+            className={`w-full flex ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"} p-2 bg-[#f5f3f6] rounded-3xl
+            dark:bg-[#101012]`}
           >
             <img
               src="/public/parking-1.png"
@@ -27,11 +42,17 @@ export default function Features() {
             />
 
             <div className="flex flex-col justify-center p-12 gap-2">
-              <span className="text-5xl font-semibold text-[#e4e2e5]">
+              <span
+                className="text-5xl font-semibold text-[#1B1B1E]
+                dark:text-white"
+              >
                 {feature.name}
               </span>
 
-              <span className="text-2xl text-[#7E8088]">
+              <span
+                className="text-2xl text-[#7E8088]
+                dark:text-[#75777E]"
+              >
                 {feature.description}
               </span>
             </div>
