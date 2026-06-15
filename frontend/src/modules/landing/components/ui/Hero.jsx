03fb4cf39e8edb7c9a51ee5d653123ae51b219f4
DevJuan001 <juanesyt7@gmail.com>
2026-06-14 20:50:11 -0500

    style(frontend): enhance Hero component with dark mode support and update text colors

--- frontend/src/modules/landing/components/ui/Hero.jsx
+++ frontend/src/modules/landing/components/ui/Hero.jsx
@@ -2,11 +2,14 @@ export default function Hero({ openModal }) {
   return (
     <section
       id="hero"
-      className="w-full my-32 flex flex-col items-center gap-4"
+      className="w-full my-32 flex flex-col items-center gap-4
+      dark:text-white"
     >
-      <h1 className="text-6xl font-semibold">Gestiona tu Parqueadero Fácil</h1>
+      <h1 className="text-6xl font-semibold dark:text-[#e4e2e5]">
+        Gestiona tu Parqueadero Fácil
+      </h1>
 
-      <p className="text-2xl text-center text-gray-500 font-medium w-xl">
+      <p className="text-2xl text-center text-[#7e8088] font-medium w-xl">
         Registra vehículos, asigna plazas, calcula tarifas automáticamente y
         administra toda tu operación desde un único panel.
       </p>
@@ -18,7 +21,8 @@ export default function Hero({ openModal }) {
             openModal(null, "register", e.currentTarget);
           }}
           className="px-7 py-3 border rounded-3xl font-semibold bg-black text-white transition-transform duration-300
-          hover:scale-[1.02] hover:text-[#ffffffb4]"
+          hover:scale-[1.02] hover:text-[#ffffffb4]
+          dark:bg-white dark:text-black dark:hover:text-black"
         >
           Prueba gratis
         </button>
@@ -29,7 +33,8 @@ export default function Hero({ openModal }) {
             openModal(null, "logIn", e.currentTarget);
           }}
           className="px-6 py-3 border rounded-3xl font-semibold transition-colors duration-300
-          hover:bg-gray-200"
+          hover:bg-gray-200
+          dark:text-[#e4e2e5] dark:border-[#202022] dark:hover:bg-[#202022]"
         >
           Iniciar sesión
         </button>
