16b202b3dedfda97b5b7add006d738cd0d0684c4
DevJuan001 <juanesyt7@gmail.com>
2026-06-14 20:52:39 -0500

    feat(frontend): create VideoSection component for landing page UI

---
+++ frontend/src/modules/landing/components/ui/VideoSection.jsx
@@ -0,0 +1,7 @@
+export default function VideoSection() {
+  return (
+    <section className="w-full flex flex-col items-center">
+      <div className="w-7xl h-[800px] bg-[#101012] rounded-3xl"></div>
+    </section>
+  );
+}
