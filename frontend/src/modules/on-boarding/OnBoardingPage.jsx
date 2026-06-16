6fc6ede7976cdec0c8532815803e3240bd052b01
DevJuan001 <juanesyt7@gmail.com>
2026-06-15 21:24:54 -0500

    feat(onboarding): create OnBoardingPage and SectionButtons components to enhance onboarding UI

---
+++ frontend/src/modules/on-boarding/OnBoardingPage.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import ProgressBar from "./components/ui/ProgressBar";
+import { useCompleteOnBoarding } from "./hooks/useCompleteOnBoarding";
+import UserInfoSection from "./components/ui/UserInfoSection";
+import ParkingNameSection from "./components/ui/ParkingNameSection";
+import ParkingLocationSection from "./components/ui/ParkingLocationSection";
+
+export default function OnBoardingPage() {
+  const [progress, setProgress] = useState(100);
+  const [activeSection, setActiveSection] = useState("userInfo");
+  const { form, loading, fieldError, handleChange, handleSubmit } =
+    useCompleteOnBoarding();
+
+  return (
+    <section className="w-screen h-screen flex flex-col items-center font-dmsans">
+      <ProgressBar progress={progress} />
+
+      <UserInfoSection
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+        setProgress={setProgress}
+        form={form}
+        handleChange={handleChange}
+        fieldError={fieldError}
+      />
+
+      <ParkingNameSection
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+        setProgress={setProgress}
+        form={form}
+        handleChange={handleChange}
+        fieldError={fieldError}
+      />
+
+      <ParkingLocationSection
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+        setProgress={setProgress}
+        form={form}
+        handleChange={handleChange}
+        fieldError={fieldError}
+        handleSubmit={handleSubmit}
+      />
+    </section>
+  );
+}
